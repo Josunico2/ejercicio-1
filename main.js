cf36a400a9ec5d5b735d7b8a2079cea3ed02ac44
Kiotoks <nikogr2005@gmail.com>
2023-03-22 11:49:26 -0300

    nose

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ function setup()
 {
   createCanvas(500, 500);   //crea un lienzo o panel donde estará el juego. El primer parámetro es el ancho y el segundo el alto del lienzo.
   laMagiaDeLosProfes();
+  ponerMinasTablero();
 
   //Asigno colores que se utilizarán. La fn color solo está definida para el setup y el draw
   COLOR_CASILLERO_CON_MINA = color("#FF0000");
@@ -34,8 +35,14 @@ function setup()
 function draw() {
   if (hizoClick == true)
   {
-    pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_SIN_MINA); //pinta el casillero clickeado. Modificar/completar
-
+    if (tieneMinaCasillero(columnaPresionada, filaPresionada)){
+      perder(); 
+    }
+    else{
+      descubrirCasillero(columnaPresionada, filaPresionada);
+      pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_SIN_MINA); //pinta el casillero clickeado. Modificar/completar
+    }
+    
 
     
     hizoClick = false;  //Indico que ya "procesé" el click del usuario. NO modificar
@@ -50,7 +57,7 @@ function ganoElJuego()
 
 function ponerMinasTablero()
 {
-  // Modificar/completar
+  ponerMinaCasillero(5,5);
 }
 
 function mostrarMinas()
