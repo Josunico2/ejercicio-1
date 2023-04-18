a863f0456e6011ae6d115bef17c6c1261aa6a841
Kiotoks <nikogr2005@gmail.com>
2023-04-18 17:05:08 -0300

    Finalizado 1.0

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function mostrarMinas()
 function contarMinasAlrededor(columna, fila)
 {
   let arr1 = [-1,0,1,1,1,0,-1,-1];
-  let arr2 = [-1,-1,-1,0,1,1,1,0,];
+  let arr2 = [-1,-1,-1,0,1,1,1,0];
 
   for(let i = 0; i < 8; i++){
     if (tieneMinaCasillero(columna + arr1[i], fila + arr2[i])){
